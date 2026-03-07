ad5413223a3851f566de1eb5e7db94a78aaec9cf
mohitchaudhary95 <mohitthe31@gmail.com>
2026-03-07 10:04:14 +0530

    6th March

---
+++ March6Assignments/Ienumerable/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ienumerable
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> emp = new List<Employee>() {
+                new Employee{ Name = "John", Id = 1, HireDate = new DateTime(2022, 5, 1) },
+                new Employee{ Name = "Adam", Id = 2, HireDate = new DateTime(2020, 10, 3) },
+                new Employee{ Name = "Dex", Id = 3, HireDate = new DateTime(2021, 12, 11) }
+            };
+
+            IEnumerable<Employee> emp2 = from e in emp where e.HireDate.Year > 2020 select e;
+
+            foreach (Employee e in emp2)
+            {
+                Console.WriteLine($"{ e.Name}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
