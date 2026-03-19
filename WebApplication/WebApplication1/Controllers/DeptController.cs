0c366324adb745e9d465f4c06fdbbd17de5b8077
mohitchaudhary95 <mohitthe31@gmail.com>
2026-03-19 15:34:40 +0530

    LayoutsAndSections

---
+++ WebApplication/WebApplication1/Controllers/DeptController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DeptController : Controller
+    {
+        List<Department> deptlist = new List<Department>()
+            {
+                new Department{DeptID=10,DeptName="Sales"},
+                new Department{DeptID=20,DeptName="HR"},
+                new Department{DeptID=30,DeptName="Software"}
+            };
+        public IActionResult Index()
+        {
+            return View(deptlist);
+        }
+    }
+}
---
+++ WebApplication/WebApplication1/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class Department
+    {
+        public int DeptID { set; get; }
+        public string? DeptName { set; get; }
+    }
+}
