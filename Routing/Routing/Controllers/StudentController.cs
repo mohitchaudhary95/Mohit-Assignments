1c23762ce55b5bb263444e16f79c793a496f1a77
mohitchaudhary95 <mohitthe31@gmail.com>
2026-03-24 09:24:13 +0530

    Routing Assignment

---
+++ Routing/Routing/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Routing.Models;
+
+namespace Routing.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1,Name="John",Class="10th" },
+        new Student{Id=2,Name="Jane",Class="9th"},
+        new Student{Id=3,Name="Doe",Class="8th"},
+new  Student{Id= 4, Name= "Smith", Class= "7th"}
+            };
+        public IActionResult GetAllStudent()
+        {
+            return View(students);
+        }
+        public IActionResult GetStudent(int id)
+        {
+            var student=students.FirstOrDefault(s=>s.Id==id);
+            return View(student);
+        }
+        public IActionResult fewcolumns()
+        {
+            var fewcolumns = students.Select(s => new { s.Name, s.Class }).ToList();
+            return View(fewcolumns);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
