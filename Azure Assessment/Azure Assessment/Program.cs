dd4aedad715aa8288387665b8645fbae4e0ce110
mohitchaudhary95 <mohitthe31@gmail.com>
2026-04-02 11:46:33 +0530

    Azure Assignment

---
+++ Azure Assessment/Azure Assessment/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Azure_Assessment
+{
+    internal class Program
+    {
+        static string str_connection = "DefaultEndpointsProtocol=https;AccountName=mohitstoragecg;AccountKey=r8OAv9j/vykz+2dNHQsH+iylwbAsJbKQNI6WGSrGqwsFA2EXUuWIAIaTWe9wZW0oxhPZj74xs42T+AStJbGKjQ==;EndpointSuffix=core.windows.net";// this value i had kept empty u have to take it from azure access keys and add it and in github u cant put sececet 
+        // connection like this becasue when u try to commit it tells secret information is there so for security reasong i cannot do commit so removed
+
+        static async Task Main()
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(str_connection);
+            BlobContainerClient container_client =
+            blobServiceClient.GetBlobContainerClient("data");
+            BlobClient blob_client = container_client.GetBlobClient("CNotes.txt");
+            using FileStream uploadFileStream = File.OpenRead(@"D:\CNotes.txt");
+            await blob_client.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+            Console.WriteLine("File uploaded");
+            Console.WriteLine("Operation complete");
+        }
+    }
+}
