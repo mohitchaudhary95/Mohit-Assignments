0cf54672f0b5e7423064c0f8dc39c3c400f0a20f
mohitchaudhary95 <mohitthe31@gmail.com>
2026-04-11 17:03:11 +0530

    Azure Function and MVC Assessment

---
+++ Week5/AzureFunctionsTangyWeb/TangyAzureFunc/Models/SalesRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TangyAzureFunc.Models
+{
+    public class SalesRequest
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Status { get; set; }
+    }
+}
